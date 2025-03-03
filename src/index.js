02446c5dd5620006ecdc89e2762f6b79f08282d5
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2025-03-03 18:31:16 +0100

    format with biome

--- src/index.js
+++ src/index.js
@@ -1,48 +1,45 @@
 //#region Show Time
-const hour = document.getElementById('hour')
-const minute = document.getElementById('minute')
-const second = document.getElementById('second')
-const milisecond = document.getElementById('milisecond')
+const hour = document.getElementById("hour");
+const minute = document.getElementById("minute");
+const second = document.getElementById("second");
+const milisecond = document.getElementById("milisecond");
 
 function update() {
-    requestAnimationFrame(update)
-    const now = new Date()
-    hour.innerText = now.getHours().toString().padStart(2, '0')
-    minute.innerText = now.getMinutes().toString().padStart(2, '0')
-    second.innerText = now.getSeconds().toString().padStart(2, '0')
-    milisecond.innerText = now.getMilliseconds().toString().padStart(3, '0')
+	requestAnimationFrame(update);
+	const now = new Date();
+	hour.innerText = now.getHours().toString().padStart(2, "0");
+	minute.innerText = now.getMinutes().toString().padStart(2, "0");
+	second.innerText = now.getSeconds().toString().padStart(2, "0");
+	milisecond.innerText = now.getMilliseconds().toString().padStart(3, "0");
 }
 
-update()
+update();
 //#endregion
 
 //#region Set font sizes
 function setFontSizes() {
-    console.log(document.getElementsByClassName('fontsize'))
-    const elements = document.getElementsByClassName('fontsize');
-    for (const element of elements) {
-        element.style.fontSize = `${element.clientHeight}px`;
-        element.style.lineHeight = `${element.clientHeight}px`;
-    }
+	console.log(document.getElementsByClassName("fontsize"));
+	const elements = document.getElementsByClassName("fontsize");
+	for (const element of elements) {
+		element.style.fontSize = `${element.clientHeight}px`;
+		element.style.lineHeight = `${element.clientHeight}px`;
+	}
 }
 
-setFontSizes()
-window.addEventListener('DOMContentLoaded', setFontSizes)
-window.addEventListener('resize', setFontSizes)
+setFontSizes();
+window.addEventListener("DOMContentLoaded", setFontSizes);
+window.addEventListener("resize", setFontSizes);
 //#endregion
 
 //#region Hide mouse
-let mouseHider = null
-window.addEventListener(
-    'mousemove',
-    () => {
-        document.body.style.cursor = 'default'
-        clearTimeout(mouseHider)
-        mouseHider = setTimeout(() => {
-            document.body.style.cursor = 'none'
-        }, 3000)
-    }
-)
+let mouseHider = null;
+window.addEventListener("mousemove", () => {
+	document.body.style.cursor = "default";
+	clearTimeout(mouseHider);
+	mouseHider = setTimeout(() => {
+		document.body.style.cursor = "none";
+	}, 3000);
+});
 
-document.body.style.cursor = 'none'
+document.body.style.cursor = "none";
 //#endregion
